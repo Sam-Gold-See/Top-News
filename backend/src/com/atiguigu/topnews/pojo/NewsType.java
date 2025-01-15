9bfcb38b964e875cd92884ae30f683618564201d
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-01-15 13:36:13 +0800

    完善所有的实体类，用于数据库对象实体类和多表查询实体类

---
+++ backend/src/com/atiguigu/topnews/pojo/NewsType.java
@@ -0,0 +1,15 @@
+package com.atiguigu.topnews.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class NewsType implements Serializable {
+    private Integer tid;
+    private String tname;
+}
